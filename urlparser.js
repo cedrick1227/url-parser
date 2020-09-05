f7fc180e467267381809282fec49843527307ed5
cedrick1227 <54310097+cedrick1227@users.noreply.github.com>
2020-09-05 14:52:34 -0700

    Update urlparser.js

--- urlparser.js
+++ urlparser.js
@@ -1,39 +1,63 @@
-const urlparser = {
-    _querystr:function(){
-        let query = location.hash.slice(location.has.indexOf('/')+1）；
-            query = query.replace('?', "")
-                         .split(',');
+const urlParser = {
+    _queryStr:()=>{
+        let query = location.hash.slice(location.hash.indexOf('/')+1);
+            query = query.replace('?',"")
+                            .split(',');
         return query;
     },
-    parse:function(){
-        if(location.hash){
+    parse:()=>{
+        if (location.hash){
             let hash, query;
             if (location.hash.includes('/')){
-                let obj = this._queryStr()
-                              .reduce((accu, iter)=>{
-                                  if (iter){
-                                      iter = iter.split('=');
-                                      iter[0] = iter[0].replace('%20', "");
-                                      iter[1] = iter[1].replace('%20', " ");
-                                      accu[iter[0]] = iter[1];
-                                  }
-                                  return accu;
-                              }, {});
-                hash = location.hash.slice(1,location.has.indexOf('/'));
-                query = obj;
+                let obj = urlParser._queryStr()
+                            .reduce((accu, iter)=>{
+                                if (iter){
+                                    iter = iter.split('=');
+                                    iter[0] = iter[0].replace('%20', "");
+                                    iter[1] = iter[1].replace('%20', " ");
+                                    accu[iter[0]] = iter[1];
+                                }
+                                return accu;
+                            },{})
+                hash=location.hash.slice(1,location.hash.indexOf('/'));
+                query=obj;
             } else {
                 hash = location.hash.slice(1);
                 query = {};
             }
-            return {hash, query};
+            return{
+                hash,
+                query,
+            }
         } else {
             return false;
         }
     },
-    query:function(){
-        let query = this.parse().hash+'/';
-        return {
-            replace
-        }
-    }
+	query:()=>{
+		let query = urlParser.parse().query;
+		return {
+			replace:(obj)=>{
+				let str = '#'+urlParser.parse().hash+'/';
+				for (let key in query){
+					if (key in obj){
+						query[key] = obj[key];   
+					}
+					str = str+`${key}=${query[key]},`
+				}
+				console.log(str)
+				history.replaceState(null, null, str);		
+			},			
+			get:(key)=>{
+				if (key){
+					return urlParser.parse()['query'][key];
+				} else {
+					return urlParser.parse()['query'];
+				}
+			},
+		}
+	},
+    replace:(hash)=>{
+		location.replace(hash);  
+		history.replaceState(null, null, hash);
+    },
 }
